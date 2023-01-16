aa7b683914c8406f83f271fcd51bd735f7d64bb5
Marlisson Silva <marlyssonsilva005@gmail.com>
2023-01-16 08:24:19 -0300

    mudança em toda estrutura do site, melhorando o mobile-first

--- js/modal.js
+++ js/modal.js
@@ -1,45 +1,46 @@
 const dataModal = [
     {
 
-        title: 'hora do dia',
+        title: 'clínico geral',
         img: './assets/logo.png',
-        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore alias accusantium excepturi id nobis velit! Reiciendis obcaecati quos, distinctio unde enim expedita. At unde modi non cum obcaecati nulla dolorem? Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore alias accusantium excepturi id nobis velit! Reiciendis obcaecati quos, distinctio unde enim expedita. At unde modi non cum obcaecati nulla dolorem? Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore alias accusantium excepturi id nobis velit! Reiciendis obcaecati quos, distinctio unde enim expedita. At unde modi non cum obcaecati nulla dolorem?excepturi id nobis velit! Reiciendis obcaecati quos, distinctio unde enim expedita. At unde modi non cum obcaecati nulla dolorem?',
+        desc: 'Um clínico geral em odontologia é um dentista que é habilitado para realizar uma ampla gama de procedimentos odontológicos. Eles podem tratar cáries, fazer limpezas dentárias, realizar cirurgias, aplicar coroas e pontes, entre outras coisas. Eles também podem prescrever medicamentos e dar orientações de cuidados bucais para os pacientes. O objetivo principal de um clínico geral em odontologia é ajudar os pacientes a manter a saúde bucal e os dentes fortes e saudáveis.',
         contato: '',
     },
     {
 
-        title: 'arte e sorriso',
+        title: 'endodontia',
         img: './assets/logo.png',
-        desc: '    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore alias accusantium excepturi id nobis velit! Reiciendis obcaecati quos, distinctio unde enim expedita. At unde modi non cum obcaecati nulla dolorem?',
+        desc: 'Endodontia é a especialidade da odontologia que se ocupa do tratamento de doenças e lesões do tecido interno do dente, conhecido como nervo ou polpa. Os procedimentos endodônticos são comumente chamados de tratamentos de canal, pois envolvem a remoção da polpa infectada ou danificada do interior do dente e o preenchimento do espaço vazio com um material de obturação. A endodontia é realizada para salvaguardar os dentes que de outra forma seriam extraídos devido à dor ou infecção.',
         contato: '',
     },
     {
 
-        title: 'Collor Flipper',
+        title: 'periodontia',
         img: './assets/logo.png',
-        desc: '    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore alias accusantium excepturi id nobis velit! Reiciendis obcaecati quos, distinctio unde enim expedita. At unde modi non cum obcaecati nulla dolorem?',
+        desc: 'Periodontia é a especialidade da odontologia que se ocupa da saúde dos tecidos de suporte dos dentes, incluindo gengivas, ligamentos e osso. A doença periodontal, também conhecida como doença gengival, é uma infecção desses tecidos que pode levar à perda dos dentes se não for tratada. Os procedimentos periodontais podem incluir limpezas profundas, cirurgias para remover o tecido infectado e tratamentos para fortalecer os tecidos de suporte. Os dentistas periodontistas são treinados para diagnosticar e tratar problemas relacionados aos tecidos de suporte dos dentes.',
         contato: '',
 
     },
     {
 
-        title: 'Order summary card',
+        title: 'prótese',
         img: './assets/logo.png',
-        desc: '    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore alias accusantium excepturi id nobis velit! Reiciendis obcaecati quos, distinctio unde enim expedita. At unde modi non cum obcaecati nulla dolorem?',
+        desc: 'A prótese dentária é um tratamento utilizado para substituir dentes perdidos ou danificados.Os materiais mais comuns utilizados na confecção de próteses são a resina acrílica, o metal e a cerâmica. O tipo de prótese mais adequado para cada paciente depende de fatores como o número de dentes perdidos, a saúde dos dentes remanescentes e da boca, e as preferências do paciente em relação ao custo e aparência.',
+        // Existem vários tipos de próteses, incluindo:Próteses removíveis: são aquelas que podem ser facilmente retiradas pelo paciente, como as próteses totais ou parciais.Próteses fixas: são aquelas que são fixadas aos dentes remanescentes ou ao osso, como as coroas, pontes e implantes dentários.Próteses parciais removíveis: são próteses que substituem alguns dentes e são mantidas no lugar por meio de ganchos que se agarram aos dentes remanescentes. Próteses totais: são próteses que substituem todos os dentes da arcada superior ou inferior.
         contato: '',
     },
     {
 
-        title: 'column preview card component',
+        title: 'implantodontia',
         img: './assets/logo.png',
-        desc: '    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore alias accusantium excepturi id nobis velit! Reiciendis obcaecati quos, distinctio unde enim expedita. At unde modi non cum obcaecati nulla dolorem?',
+        desc: 'Implantodontia é a especialidade da odontologia que se ocupa da colocação de implantes dentários e da reconstrução dos dentes com base em implantes. Os implantes dentários são pinos de titânio ou outro material que são inseridos no osso da mandíbula ou maxila como substitutos dos raízes dos dentes. Eles servem como base para a fixação de coroas, pontes ou próteses removíveis.A colocação de implantes é um procedimento cirúrgico que é realizado por um cirurgião oral ou por um dentista especialmente treinado em implantodontia. Depois que os implantes são colocados, eles precisam de tempo para se integrar ao osso antes que possam ser usados para suportar a reconstrução dos dentes. A implantodontia é uma opção de tratamento eficaz para a substituição de dentes perdidos ou danificados e pode ajudar a melhorar a aparência e a função dos dentes. No entanto, é importante notar que os implantes dentários nem sempre são uma opção viável para todos os pacientes, dependendo da saúde geral e da quantidade de osso disponível na mandíbula ou maxila.',
         contato: '',
     },
     {
 
-        title: 'IMC',
+        title: 'ortodontia',
         img: './assets/logo.png',
-        desc: '    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore alias accusantium excepturi id nobis velit! Reiciendis obcaecati quos, distinctio unde enim expedita. At unde modi non cum obcaecati nulla dolorem?',
+        desc: 'Ortodontia é a especialidade da odontologia que se ocupa do diagnóstico, prevenção e tratamento de problemas de alinhamento dos dentes e da mandíbula. Os ortodontistas são dentistas que se especializam em corrigir más oclusões (erros de mordida), como apinhamento, overlap e espaços entre os dentes. Eles fazem isso usando aparelhos ortodônticos, como brackets, arcos e placas de expansão. O tratamento ortodôntico pode ser realizado em pessoas de todas as idades, embora seja mais comum em crianças e adolescentes. O objetivo da ortodontia é melhorar a aparência dos dentes e a função da mandíbula, bem como prevenir problemas de saúde bucal futuros. No entanto, o tratamento ortodôntico pode ser um processo longo e, em alguns casos, pode ser bastante invasivo.',
         contato: '',
     }
 
@@ -60,7 +61,9 @@ cards.forEach(card => {
     card.addEventListener('click', () => {
         fade.classList.toggle('active')
         modal.classList.toggle('active')
-        document.body.style.overflow = 'hidden'
+        document.body.style.overflowY = 'hidden'
+        btnWhats.style.display = 'none'
+
     });
 });
 
@@ -69,7 +72,8 @@ cards.forEach(card => {
     e.addEventListener('click', () => {
         fade.classList.toggle('active')
         modal.classList.toggle('active')
-        document.body.style.overflow = 'visible'
+        document.body.style.overflowY = 'visible'
+        btnWhats.style.display = 'block'
     })
 })
 
@@ -77,8 +81,6 @@ cards.forEach(card => {
 cards.forEach(card => {
     card.addEventListener('click', () => {
         const index = card.getAttribute('data-index');
-        console.log(`${index}`);
-        console.log(dataModal[index])
         showModal(index);
     });
 });
@@ -86,11 +88,11 @@ cards.forEach(card => {
 
 
 function showModal(index) {
-    const 
-    item = dataModal[index]
+    const
+        item = dataModal[index]
     img.src = item.img;
     title.textContent = item.title;
     desc.textContent = item.desc;
     contato.href = item.contato;
-  
+
 }
--- js/script.js
+++ js/script.js
@@ -1,60 +1,87 @@
-// MENU MOBILE
-const btnMobile = document.querySelector('#menu_hamburguer')
+const btnMobile = document.querySelector('#btn-mobile')
 function showMenu(event) {
-      if (event.type === 'touchstart') event.preventDefault()
-      const nav = document.querySelector('.nav')
-      nav.classList.toggle('active')
-      document.body.style.overflow = 'hidden'
+    if (event.type === 'touchstart') event.preventDefault()
+    const nav = document.querySelector('.nav')
+    nav.classList.toggle('active')
+    if (nav.classList.contains('active')) {
+        document.body.style.overflow = 'hidden'
+    } else {
+        document.body.style.overflow = 'visible'
+    }
+
 }
 btnMobile.addEventListener('touchstart', showMenu)
 btnMobile.addEventListener('click', showMenu)
 
-// HIDE MENU ON CLICK
 const menuItems = document.querySelectorAll('.menu a');
 
 menuItems.forEach(item => {
-      item.addEventListener('click', hideMenu);
+    item.addEventListener('click', hiddenMenu);
 })
 
-function hideMenu(event) {
-      if (event.type === 'touchstart') event.preventDefault()
-      const nav = document.querySelector('.nav')
-      nav.classList.remove('active')
+function hiddenMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault()
+    const nav = document.querySelector('.nav')
+    nav.classList.remove('active')
+    document.body.style.overflow = 'visible'
 }
 
-
-// SCROLL MENU
 menuItems.forEach(item => {
-      item.addEventListener('click', scrollClick);
+    item.addEventListener('click', scrollClick);
 })
 
 function scrollClick(event) {
-      event.preventDefault();
-      const element = event.target;
-      const id = element.getAttribute('href')
-      const to = document.querySelector(id).offsetTop;
+    event.preventDefault();
+    const element = event.target;
+    const id = element.getAttribute('href')
+    const to = document.querySelector(id).offsetTop;
 
-      window.scroll({
-            top: to - 65,
-            behavior: "smooth",
-      })
+    window.scroll({
+        top: to - 65,
+        behavior: "smooth",
+    })
 }
 
-// 
+// ========================================================
+
+// ========================================================
+
+// botao do whatsap
+
+let btnWhats = document.querySelector('#btn-whats img');
+
+function hover(event) {
+    if (event.type === 'mouseenter');
+    const textWhats = document.getElementById('text-whats');
+    textWhats.style.visibility = 'visible';
+    textWhats.style.transition = '.6s';
+}
+function removeHover(event) {
+    if (event.type === 'mousedown');
+    const textWhats = document.getElementById('text-whats');
+    textWhats.style.visibility = 'hidden';
+    textWhats.style.transition = '.3s';
+
+}
+
+btnWhats.addEventListener('mouseenter', hover);
+btnWhats.addEventListener('mouseout', removeHover)
+
+// ========================================================
 
 const debounce = function (func, wait, immediate) {
-      let timeout;
-      return function (...args) {
-            const context = this;
-            const later = function () {
-                  timeout = null;
-                  if (!immediate) func.apply(context, args);
-            };
-            const callNow = immediate && !timeout;
-            clearTimeout(timeout);
-            timeout = setTimeout(later, wait);
-            if (callNow) func.apply(context, args)
-      };
+    let timeout;
+    return function (...args) {
+        const context = this;
+        const later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        const callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args)
+    };
 };
 
 
@@ -62,54 +89,28 @@ const target = document.querySelectorAll('[data-anime]');
 const animationClass = 'animate';
 
 function animeScroll() {
-      const windowTop = window.pageYOffset + (window.innerHeight * 0.75); // sabendo a altura da pagina
-      target.forEach(function (element) {
-            if ((windowTop) > element.offsetTop) {
-                  element.classList.add(animationClass)
-            }
-            else {
-                  element.classList.remove(animationClass)
-            }
-      })
+    const windowTop = window.pageYOffset + (window.innerHeight * 0.65); // sabendo a altura da pagina
+    target.forEach(function (element) {
+        if ((windowTop) > element.offsetTop) {
+            element.classList.add(animationClass)
+        } else {
+            element.classList.remove(animationClass)
+        }
+    })
 }
 
 animeScroll() // garantir que a funçao seja executada logo ao entrar na página
 
 
 if (target.length) { // verificar se tem itens dentro do target
-      window.addEventListener('scroll', debounce(function () {
-            animeScroll()
-      }, 20));
+    window.addEventListener('scroll', debounce(function () {
+        animeScroll()
+    }, 20));
+
 
 }
 
-// let btnWhats = document.querySelector('#btn-whats img');
 
-// function hover(event) {
-//       if (event.type === 'mouseenter');
-//       const textWhats = document.getElementById('text-whats');
-//       textWhats.style.visibility = 'visible';
-//       textWhats.style.transition = '.6s';
-// }
-// function removeHover(event) {
-//       if (event.type === 'mousedown');
-//       const textWhats = document.getElementById('text-whats');
-//       textWhats.style.visibility = 'hidden';
-//       textWhats.style.transition = '.3s';
-
-// }
-
-// btnWhats.addEventListener('mouseenter', hover);
-// btnWhats.addEventListener('mouseout', removeHover)
-
-
-
-// document.getElementById('contatoForm').addEventListener('submit', function () {
-//       var nome = this.querySelector('input[name=nome]'), nome = nome.value;
-//       var email = this.querySelector('input[name=email]'), email = email.value;
-//       var texto = ''+ email;
-//       this.querySelector('input[name=Body]').setAttribute('value', texto);
-//   });
 
 
 
